2a947363ebf90c111bd8e1b3731d9de4ae309ea3
Sang Le <lqsang1995@gmail.com>
2025-02-15 15:29:23 +0700

    Fix API gateway 9

--- elevator-server/src/main.ts
+++ elevator-server/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,8 @@ async function bootstrap() {
     origin: ['*', 'https://pr-elevator-client.vercel.app'],
     methods: 'GET, PUT, POST, DELETE'
   })
+
+  app.useWebSocketAdapter(new IoAdapter(app));
 
   await app.listen(process.env.PORT ?? 3001);
 }
