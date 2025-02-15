658949a932601bd62705f816e4e67ad27c8ae13c
Sang Le <lqsang1995@gmail.com>
2025-02-15 12:29:10 +0700

    Fix API host 1

--- elevator-client/src/api/elevator.ts
+++ elevator-client/src/api/elevator.ts
@@ -3,7 +3,7 @@ import { iElevator } from "@/types/elevator"
 export const initialElevator = async (): Promise<iElevator[]> => {
     console.log('process.env.API_HOST= === ', process.env.API_HOST)
 
-    const data = await fetch(`${process.env.API_HOST}/elevator/initial`)
+    const data = await fetch(`${process.env.API_HOST || 'https://pr-elevator.vercel.app'}/elevator/initial`)
     const result = await data.json()
 
     return result
