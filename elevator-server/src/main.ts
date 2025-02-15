d39f2ddac05831b3c8cc0427df8fc5fc16fe344c
Sang Le <lqsang1995@gmail.com>
2025-02-15 14:17:57 +0700

    Fix API gateway 5

--- elevator-server/src/main.ts
+++ elevator-server/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://pr-elevator.vercel.app', 'https://pr-elevator-client.vercel.app/'],
+    origin: '*',
     methods: 'GET, PUT, POST, DELETE'
   })
 
